7ad050a223ea67a694d5a62eddb2af8052fbc6fd
Theodora <57034124+thwreo@users.noreply.github.com>
2021-09-25 21:46:26 +0300

    Add files via upload

--- Calendar.js
+++ Calendar.js
@@ -14,13 +14,28 @@ function windowsClock() {
 }
 
 function windowsDate() {
-    var date = new Date();
-    day = date.getDate();
-    month = date.getMonth();
-    year = date.getFullYear();
-    const monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"];
-    month = monthNames[month];
+    var lang = navigator.language;
+    var event = new Date();
+    const option_day = { day: 'numeric' };
+    const option_month = { month: 'long' };
+    const option_year = { year: 'numeric' };
+    day = event.toLocaleDateString(lang, option_day);
+    month = event.toLocaleDateString(lang, option_month);
+    year = event.toLocaleDateString(lang, option_year);
+    // var date = new Date();
+    // day = date.getDate();
+    // month = date.getMonth();
+    // year = date.getFullYear();
+    // const monthNames = ["January", "February", "March", "April", "May", "June",
+    // "July", "August", "September", "October", "November", "December"];
+    // month = monthNames[month];
     document.getElementById('date').innerHTML = day + " " + month + " " + year;
     document.getElementById('underDate').innerHTML = month + " " + year;
+}
+
+function windowsCalendar() {
+    var date = new Date();
+    month = date.getMonth();
+    year = date.getFullYear();
+    document.getElementById('cal').innerHTML = createCalendar(cal, year, month);
 }
